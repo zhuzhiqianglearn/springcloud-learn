f8eed655b3c094398e3c8b1593212c0d3fdef22c
zhu zhiqiang <zhuzhiqiang@dachuizichan.com>
2019-01-09 12:01:53 +0800

    exchange 参数说明

--- cloudmain-Eureka/rabbitmq/src/test/java/RabbitmqHello/SenderHello.java
+++ cloudmain-Eureka/rabbitmq/src/test/java/RabbitmqHello/SenderHello.java
@@ -4,6 +4,7 @@ import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.MessageProperties;
+import sun.awt.CausedFocusEvent;
 
 import java.io.IOException;
 import java.util.Queue;
@@ -27,9 +28,11 @@ public class SenderHello {
         Connection connection=factory.newConnection();//创建连接
         Channel channel = connection.createChannel();//创建信道
         //创建一个type="direct",持久化的，非自定删除的交换器
+
         channel.exchangeDeclare(EXCHANG_NAME,"direct",true,false,null);
         //创建一个持久化，非排他的，非自定删除的队列
         channel.queueDeclare(QUEUE_NAME,true,false,false,null);
+        //String queue = channel.queueDeclare().getQueue(); 创建一个同一个connection层面可用，应用断开连接，就自动删除
         //将交换器与队列通过路由键绑定
         //相当于包裹的目的地
         channel.queueBind(QUEUE_NAME,EXCHANG_NAME,ROUTING_KEY);
@@ -53,7 +56,7 @@ public class SenderHello {
         //关闭资源
         channel.close();;
         connection.close();
-
+   channel.isOpen();
         //交换器 常用的累心共有 fanout，direct，topic,headers 四种
         /*
         * fanout：把所有发送到该交换器的消息路由到所有与交换器绑定的队列中
@@ -61,5 +64,23 @@ public class SenderHello {
         *topic： 可以匹配bingdingKey 和RoutingKey  匹配符号 .分隔符  *匹配单个单词  #匹配多个词可以为0 （例如 *.abcd.*;*.*.abcd,abcd.#）
         * headers：依据发送消息时候的headers信息
         * */
+
+        /*判断 connection 和channel 是否开启， 可以用 isOpen ,不建议在生产环境上使用，返回值 依赖于 shutdownCause,可能存在竞争
+        public boolean isOpen() {
+            Object var1 = this.monitor;
+            synchronized(this.monitor) {
+                return this.shutdownCause == null;
+            }
+        }
+
+        exchangeDeclare参数说明：
+        exchange ：交换器名称
+        type:交换器类型（常用：fanout，direct，topic,headers）
+        durable:设置是否持久化，true，持久化到硬盘，重启不会丢失信息
+        autoDelete：设置是否自动删除
+        internal:设置是否是内置的，ture 表示内置交换器，客户端无法直接发送消息到这个交换器中，只能通过交换器路由到交换器这种方式
+        argument(Map):其他一些结构化参数
+         * */
     }
+
 }
