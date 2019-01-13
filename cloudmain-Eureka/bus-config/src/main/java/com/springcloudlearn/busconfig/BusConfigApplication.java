3e24e5bf6d161d664bd4e1a1c1b68e6210015ee3
zhu zhiqiang <zhuzhiqiang@dachuizichan.com>
2019-01-13 12:13:49 +0800

    config 配置

---
+++ cloudmain-Eureka/bus-config/src/main/java/com/springcloudlearn/busconfig/BusConfigApplication.java
@@ -0,0 +1,14 @@
+package com.springcloudlearn.busconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusConfigApplication.class, args);
+	}
+
+}
+
